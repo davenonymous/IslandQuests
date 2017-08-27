a8e4eb2c15e778b2fe6c8306e8e85410497901bd
thraaawn <david.hilberath@gmx.de>
2017-08-27 12:47:10 +0200

    Fixed import

--- src/main/java/org/dave/islandquests/IslandQuests.java
+++ src/main/java/org/dave/islandquests/IslandQuests.java
@@ -1,20 +1,19 @@
 package org.dave.islandquests;
 
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.dave.islandquests.init.Dimensions;
-import org.dave.islandquests.locking.PlayerEvents;
 import org.dave.islandquests.configuration.ConfigurationHandler;
+import org.dave.islandquests.init.Dimensions;
 import org.dave.islandquests.islands.IslandChunkRegistry;
 import org.dave.islandquests.islands.IslandTypeRegistry;
+import org.dave.islandquests.locking.PlayerEvents;
 import org.dave.islandquests.world.VoidIslandsEvents;
 import org.dave.islandquests.world.VoidIslandsSavedData;
 
-@Mod(modid = IslandQuests.MODID, version = IslandQuests.VERSION, name = "IslandType Quests", acceptedMinecraftVersions = "[1.12,1.13)")
+@Mod(modid = IslandQuests.MODID, version = IslandQuests.VERSION, name = "Island Quests", acceptedMinecraftVersions = "[1.12,1.13)")
 public class IslandQuests {
     public static final String MODID = "islandquests";
     public static final String VERSION = "1.0.0";
